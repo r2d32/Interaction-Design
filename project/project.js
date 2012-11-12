9b88a7be795185d9e73da8fb310affa14b5bdd56
Carlos <carlos_akemolo@hotmail.com>
2012-11-11 22:06:25 -0800

    added some more project modifications

--- project/project.js
+++ project/project.js
@@ -3,7 +3,7 @@ $(function() {
         stop: function() {
             var result = $( "#select-result" ).empty();
             $( ".ui-selected", this ).each(function() {
-                var index = $( "#selectable li" ).index( this );
+                var index = $( "#selectable li" ).index( $( ".ui-selected" ));
                 result.append( "Project number " + ( index + 1 ) );
             });
         }
